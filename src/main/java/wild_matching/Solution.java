a424aaa1d8632cb5eb364f67caa78ba981db0687
weishubin <weishubin@meituan.com>
2018-11-13 22:12:20 +0800

    WildcardMatching

--- src/main/java/wild_matching/Solution.java
+++ src/main/java/wild_matching/Solution.java
@@ -1,265 +1,90 @@
 package wild_matching;
 
-import java.util.*;
+import java.util.Map;
 
 /**
- * Created by weishubin on 2018/11/9.
+ * leetcode测试题目里没有测试这种场景："zaacbz", "*a?b*
+ * 这个代码里也暂没实现
+ * Created by weishubin on 2018/11/13.
  */
 public class Solution {
+
     public boolean isMatch(String s, String p) {
-        p = mergeStar(p);
-//        System.out.println(p);
-        Map<Integer, Pos>  groupMap = group(p);
-//        System.out.println(groupMap);
-        if (!createGroupIndex(s, p, groupMap)) {
-            return false;
-        }
-//        System.out.println(groupMap);
-        if (!mergeQuesionMark(s, p, groupMap)) {
-            return false;
-        }
-
-//        System.out.println(groupMap);
-        if(!cut(groupMap)) {
-            return false;
-        }
-//        System.out.println(groupMap);
-
-        return isMatch(s, 0, s.length(), p, 0, p.length(), groupMap) ;
-
+        return this.isMatch(false, s, 0, s.length(), p, 0, p.length());
     }
 
+    private boolean isMatch(boolean preIsWild, String s, int sStart, int sEnd, String p, int pStart, int pEnd) {
+        if (sStart < 0 || pStart < 0) {
+            return false;
+        }
 
-    private boolean isMatch(String s, int sStart, int sEnd, String p, int pStart, int pEnd, Map<Integer, Pos>  groupMap) {
         if (sStart == sEnd && pStart == pEnd) {
             return true;
-        } else if (sStart == sEnd) {
+        } else if (sStart > sEnd || pStart > pEnd) {
+            return false;
+        }
+
+        if (pStart == pEnd) { //aa,a
+            if (sStart < sEnd) {
+                return false;
+            }
+        }
+
+        if (sStart == sEnd && pStart < pEnd) {
             for (int i = pStart; i < pEnd; i++) {
                 if (p.charAt(i) != '*') {
                     return false;
                 }
             }
             return true;
-        } else if (pStart == pEnd) {
-            return false;
         }
+
 //        System.out.println(s.substring(sStart, sEnd) + "\n" + p.substring(pStart, pEnd) + "\n");
-        Pos pos = groupMap.get(pStart);
-        if (pos == null) {
-            throw new RuntimeException("pos error");
+
+        char pStartChar = p.charAt(pStart);
+        CharType charType = CharType.convert(pStartChar);
+
+        int pGroupEnd = pStart + 1;
+        while (pGroupEnd < pEnd) {
+            char c = p.charAt(pGroupEnd);
+            CharType cT = CharType.convert(c);
+            if (cT == charType) {
+                pGroupEnd++;
+            } else {
+                break;
+            }
         }
 
-        if (pos.type == CharType.Wild) {
-            if (pos.start + pos.len == pEnd) { //*是最后一个字符
-                return true;
-            } else {
-                Pos nextPos = groupMap.get(pos.start + pos.len);
-                int lastIndex = p.lastIndexOf('*');
-                if (lastIndex == pStart) { //最后一组需要匹配
-                    int sS = s.length() - p.length() + lastIndex + 1;
-                    if (sS < sStart) {
-                        return false;
-                    }
-                    return isMatch(s, sS, s.length(), p, lastIndex + 1, p.length(), groupMap);
-                } else {
-                    if (nextPos.type == CharType.Normal) { //字符
+        String sub = p.substring(pStart, pGroupEnd);
 
-                        for (int i : nextPos.matchIndex) {
-                            if (i >= sStart && i + nextPos.len <= sEnd) {
-                                return isMatch(s, i + nextPos.len, sEnd, p, nextPos.start + nextPos.len, pEnd, groupMap);
-                            }
-                        }
-                        return false;
-                    } else if (nextPos.type == CharType.Single) { //?
-                        if (nextPos.matchIndex.size() > 0) {
-                            for (int m : nextPos.matchIndex) {
-                                if (m >= sStart && m + nextPos.len <= sEnd) {
-                                    return isMatch(s, m + nextPos.len, sEnd, p, nextPos.start + nextPos.len, pEnd, groupMap);
-                                }
-                            }
-                            return false;
-                        } else {
-                            //直接匹配头几个字符
-                            return isMatch(s, sStart + nextPos.len, sEnd, p, nextPos.start + nextPos.len, pEnd, groupMap);
-                        }
-                    } else {
-                        System.err.println("error1");
-                        return false;
-                    }
+        if (charType == CharType.Wild) {
+            int lastIndex = p.lastIndexOf('*');
+            if (lastIndex < pGroupEnd) { //最后一个*，从后边匹配
+                int ss = s.length() - p.length() + lastIndex + 1;
+                if (ss < sStart) {
+                    return false;
                 }
+                return isMatch(false, s,  ss, sEnd, p, lastIndex + 1, pEnd);
+            } else {
+                return isMatch(true, s, sStart, sEnd, p, pGroupEnd, pEnd);
             }
-        } else if(pos.type == CharType.Single) {
-            if (pos.matchIndex.size() > 0) { //?号右边是字母
-                for (int m : pos.matchIndex) {
-                    if (m == sStart) {
-                        return isMatch(s, sStart + pos.len, sEnd, p, pStart + pos.len, pEnd, groupMap);
-                    }
-                }
+        } else if (charType == CharType.Normal) {
+            int index = s.indexOf(sub, sStart);
+            if (index < 0) {
                 return false;
             } else {
-                return isMatch(s, sStart + pos.len, sEnd, p, pStart + pos.len, pEnd, groupMap);
-            }
-        } else { //字符
-            for (int index : pos.matchIndex) {
-                if (index == sStart) {
-                    return isMatch(s, sStart + pos.len, sEnd, p, pStart + pos.len, pEnd, groupMap);
-                }
-            }
-            return false;
-        }
-    }
-
-    private String mergeStar(String p) {
-        //优化p，多个连续的*合并成一个
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < p.length(); i++) {
-            char c = p.charAt(i);
-            if (c != '*') {
-                sb.append(c);
-            } else {
-                if (i == 0) {
-                    sb.append(c);
-                } else if (p.charAt(i - 1) != '*') {
-                    sb.append(c);
-                }
-            }
-        }
-        return sb.toString();
-    }
-
-    /**
-     * 分组
-     */
-    private Map<Integer, Pos>  group(String p) {
-        Map<Integer, Pos> parseResult = new HashMap<Integer, Pos>();
-        if (p.length() == 0) {
-            return parseResult;
-        }
-        StringBuilder sb = new StringBuilder();
-        CharType preCharType = null; //init
-        for (int i = 0; i < p.length(); i++) {
-            char c = p.charAt(i);
-            CharType t = CharType.convert(c);
-            if (t == preCharType) {
-                sb.append(c);
-            } else {
-                if (preCharType != null && sb.length() > 0) {
-                    String s = sb.toString();
-                    int start = i - s.length();
-                    Pos pos = new Pos(preCharType, start, s.length());
-                    parseResult.put(start, pos);
-                }
-
-
-                sb.setLength(0);
-                sb.append(c);
-                preCharType = t;
-            }
-        }
-        if (sb.length() > 0) {
-            String s = sb.toString();
-            int start = p.length() - s.length();
-            Pos pos = new Pos(preCharType, start, s.length());
-            parseResult.put(start, pos);
-        }
-        return parseResult;
-    }
-
-    private boolean createGroupIndex(String s, String p, Map<Integer, Pos>  groupMap ) {
-        for (Pos pos : groupMap.values()) {
-            if (pos.type == CharType.Normal) {
-                String group = p.substring(pos.start, pos.start + pos.len);
-                int beginIndx = 0;
-                int index = s.indexOf(group, beginIndx);
-                while (index >= 0) {
-                    pos.matchIndex.add(index);
-                    if (index + 1 >= s.length()) {
-                        break;
-                    }
-                    index = s.indexOf(group, index + 1);
-                }
-                if (pos.matchIndex.size() == 0) {
+                if (!preIsWild && index != sStart) {
                     return false;
                 }
+                return isMatch(false, s, index + sub.length(), sEnd, p, pStart + sub.length(), pEnd);
+
             }
+        } else {
+            return isMatch(preIsWild, s, sStart + sub.length(), sEnd, p, pGroupEnd, pEnd);
         }
-        return true;
+
     }
 
-    /**
-     * 分析"?"
-     * @param s
-     * @param p
-     * @param groupMap
-     */
-    private boolean mergeQuesionMark(String s, String p, Map<Integer, Pos>  groupMap) {
-        Set<Integer> startSet = groupMap.keySet();
-        List<Integer> sortedList = new ArrayList<Integer>(startSet);
-        Collections.sort(sortedList);
-        for (int i = 0; i < sortedList.size(); i++) {
-            Pos pos = groupMap.get(sortedList.get(i));
-            CharType rightType = CharType.LineEnd;//$
-            Pos rightPos = null;
-            if (pos.type != CharType.Single) {
-                continue;
-            }
-            if (i < sortedList.size() - 1) {
-                int rightIndex = sortedList.get(i + 1);
-                rightPos = groupMap.get(rightIndex);
-                rightType = rightPos.type;
-            }
-
-            if (rightType == CharType.Single) {
-                System.err.println("merge error!");
-                return false;
-            }
-
-            if (rightType == CharType.Normal) {
-                boolean find = false;
-                for (int rightM : rightPos.matchIndex) {
-                    if (rightM - pos.len >= 0) {
-                        find = true;
-                        pos.matchIndex.add(rightM - pos.len);
-                    }
-                }
-                if (!find) {
-                    return false;
-                }
-                pos.len = pos.len + rightPos.len;
-            } else if (rightType == CharType.LineEnd) {
-                if (s.length() - pos.len >= 0) {
-                    pos.matchIndex.add(s.length() - pos.len);
-                } else {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-
-    static class Pos {
-        Pos(CharType t, int s, int len) {
-            this.type = t;
-            this.start = s;
-            this.len = len;
-        }
-        CharType type; //0-*,1-?,2-other
-        int start;//在p字符串中的开始位置
-        int len;//长度
-        List<Integer> matchIndex = new ArrayList(); //匹配位置
-
-        public String toString() {
-            StringBuilder sb = new StringBuilder();
-            sb.append(type);
-            sb.append('-');
-            sb.append(start);
-            sb.append('-');
-            sb.append(len);
-            sb.append('-');
-            sb.append(matchIndex);
-            return sb.toString();
-        }
-    }
 
     enum CharType {
         Wild(0),
@@ -292,69 +117,28 @@ public class Solution {
             return "c";
         }
     }
-    private boolean cut(Map<Integer, Pos>  groupMap) {
-        List<Integer> groupStartList = new ArrayList<Integer>(groupMap.keySet());
-        Collections.sort(groupStartList);
-
-        //后边的应该大于前边的最小值；前边的应该小于后边的最大值
-        int min = 0;
-        for (int  groupStart : groupStartList) {
-            List<Integer> matchIndex = groupMap.get(groupStart).matchIndex;
-            if (matchIndex.size() > 0 &&
-                    (groupMap.get(groupStart).type == CharType.Normal || groupMap.get(groupStart).type == CharType.Single)) {
-                Collections.sort(matchIndex);
-                Iterator<Integer> iter = matchIndex.iterator();
-                 while (iter.hasNext()) {
-                    if (iter.next() < min) {
-                        iter.remove();
-                    }
-                }
-
-                if (matchIndex.size() == 0) {
-                    return false;
-                }
-                min = matchIndex.get(0);
-//                System.out.println(matchIndex.size());
-            }
-        }
-
-//        System.out.println("------");
-
-        int preMax = Integer.MAX_VALUE;
-        for (int i = groupStartList.size() - 1; i >= 0; i--) {
-            int groupStart = groupStartList.get(i);
-            List<Integer> matchIndex = groupMap.get(groupStart).matchIndex;
-            if (matchIndex.size() > 0 &&
-                    (groupMap.get(groupStart).type == CharType.Normal || groupMap.get(groupStart).type == CharType.Single)) {
-                Iterator<Integer> iter = matchIndex.iterator();
-                while (iter.hasNext()) {
-                    if (iter.next() > preMax) {
-                        iter.remove();
-                    }
-                }
-
-                if (matchIndex.size() == 0) {
-                    return false;
-                }
-                preMax = matchIndex.get(matchIndex.size() - 1);
-//                System.out.println(matchIndex.size());
-            }
-        }
-
-        return true;
-    }
 
 
 
     public static void main(String[] args) {
         Solution s = new Solution();
         long t = System.currentTimeMillis();
+
+
+//        assert s.isMatch("zaacbz", "*a?b*") == true;
+        assert s.isMatch("zacabz", "*a?b*") == false;
+        assert s.isMatch("ippi", "*?i*pi") == false;
+        assert s.isMatch("mississippi", "m??*ss*?i*pi") == false;
+        assert s.isMatch("bbabab", "**?a*") == true;
+        assert s.isMatch("aa", "a") == false;
+        assert s.isMatch("", "*") == true;
+        assert s.isMatch("babaaababaabababbbbbbaabaabbabababbaababbaaabbbaaab", "***bba**a*bbba**aab**") == true;
+        assert s.isMatch("b", "??") == false;
+        assert s.isMatch("abce", "abc*??") == false;
         assert s.isMatch("aaaa", "***a") == true;
         assert s.isMatch("abaab", "*?a?") == true;
         assert s.isMatch("aaaba", "*?*?a") == true;
-        assert s.isMatch("abce", "abc*??") == false;
         assert s.isMatch("babaaababaabababbbbbbaabaabbabababbaababbaaabbbaaab", "***bba**a*bbba**aab**b") == false;
-        assert s.isMatch("babaaababaabababbbbbbaabaabbabababbaababbaaabbbaaab", "***bba**a*bbba**aab**") == true;
         assert s.isMatch("abbbba", "a**a*?") == false;
         assert s.isMatch("abc", "abc?*") == false;
         assert s.isMatch("abcd", "ab*cd") == true;
@@ -365,16 +149,15 @@ public class Solution {
         assert s.isMatch("a", "a") == true;
         assert s.isMatch("a", "a") == true;
         assert s.isMatch("", "*") == true;
-//        s.isMatch("abbabaaabbabbaababbabbbbbabbbabbbabaaaaababababbbabababaabbababaabbbbbbaaaabababbbaabbbbaabbbbababababbaabbaababaabbbababababbbbaaabbbbbabaaaabbababbbbaababaabbababbbbbababbbabaaaaaaaabbbbbaabaaababaaaabb",
-//                "**aa*****ba*a*bb**aa*ab****a*aaaaaa***a*aaaa**bbabb*b*b**aaaaaaaaa*a********ba*bbb***a*ba*bb*bb**a*b*bb");
+        s.isMatch("abbabaaabbabbaababbabbbbbabbbabbbabaaaaababababbbabababaabbababaabbbbbbaaaabababbbaabbbbaabbbbababababbaabbaababaabbbababababbbbaaabbbbbabaaaabbababbbbaababaabbababbbbbababbbabaaaaaaaabbbbbaabaaababaaaabb",
+                "**aa*****ba*a*bb**aa*ab****a*aaaaaa***a*aaaa**bbabb*b*b**aaaaaaaaa*a********ba*bbb***a*ba*bb*bb**a*b*bb");
         assert s.isMatch("acdcb", "a*c?b") == false;
         assert s.isMatch("abbabaaabbabbaababbabbbbbabbbabbbabaaaaababababbbabababaabbababaabbbbbbaaaabababbbaabbbbaabbbbababababbaabbaababaabbbababababbbbaaabbbbbabaaaabbababbbbaababaabbababbbbbababbbabaaaaaaaabbbbbaabaaababaaaabb",
                 "**aa*****ba*a*bb**aa*ab****a*aaaaaa***a*aaaa**bbabb*b*b**aaaaaaaaa*a********ba*bbb***a*ba*bb*bb**a*b*bb") == false;
-                assert s.isMatch("", "a") == false;
+        assert s.isMatch("", "a") == false;
         assert s.isMatch("cb", "*a") == false;
         assert s.isMatch("aa", "*") == true;
         assert s.isMatch("b", "?b") == false;
-        assert s.isMatch("aa", "a") == false;
         assert s.isMatch("adceb", "*a*b") == true;
         s.isMatch("", "???");
 
